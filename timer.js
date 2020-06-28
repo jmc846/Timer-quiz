4d5ddbf1f34d485ed6d1c41293246e5c7e666781
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-06-28 13:48:13 -0400

    changes to functions

--- timer.js
+++ timer.js
@@ -1,25 +1,12 @@
-
-var counter = 0
-var startTimer = select('#timer');
-timer.html('0')
-
-function timeIt(){
-counter ++;
-timer.html(counter);
-
-}
-setInterval(timeIt, 30000);
-var minutesDisplay = document.getElementById("minutes");
+// setInterval(timeIt, 30000);{};
 var secondsDisplay = document.getElementById("seconds");
-var workMinutesInput = document.getElementById("work-minutes");
-var restMinutesInput = document.getElementById("rest-minutes");
-var inputs = document.getElementById(".inputs")
-
 var totalSeconds = 0;
 var secondsElapsed = 0;
 var score
-var questions
-
+var questions = []
+var BeginEl = document.querySelector("#Begin");
+var scoreEl = document.querySelector("#score");
+BeginEl.addEventListener('click', setTime);
 // These two functions are just for making sure the numbers look nice for the html elements
 function getFormattedMinutes() {
   //
@@ -52,35 +39,47 @@ function getFormattedSeconds() {
   return formattedSeconds;
 }
 
+function begin (){
+ setTime()
+
+}
+
 /* This function retrieves the values from the html input elements; Sort of
 getting run in the background, it sets the totalSeconds variable which
 is used in getFormattedMinutes/Seconds() and the renderTime() function.
 It essentially resets our timer */
 function setTime() {
-  var minutes;
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left until Quiz over";
 
-  if (status === "Working") {
-    minutes = workMinutesInput.value.trim();
-  } else {
-    minutes = restMinutesInput.value.trim();
-  }
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
 
-  clearInterval(interval);
-  totalSeconds = minutes * 60;
-}
+  }, 60000);
+
+ function sendMessage(){
+  timeEl.textContent = "YOU LOSE"
+
+ }
+
+  // clearInterval(interval);
+  // totalSeconds =  
+
 
 // This function does 2 things. displays the time and checks to see if time is up.
 function renderTime() {
   // When renderTime is called it sets the textContent for the timer html...
-  minutesDisplay.textContent = getFormattedMinutes();
-  secondsDisplay.textContent = getFormattedSeconds();
+    secondsDisplay.textContent = getFormattedSeconds();
 
   // ..and then checks to see if the time has run out
   if (secondsElapsed >= totalSeconds) {
-    if (status === "Working") {
-      alert("Time for a break!");
+    if (status === "Keep going") {
+      alert("Quiz Master!");
     } else {
-      alert("Time to get back to work!");
+      alert("AWWW so sorry, try again!");
     }
 
     stopTimer();
@@ -90,7 +89,7 @@ function renderTime() {
 // This function is where the "time" aspect of the timer runs
 // Notice no settings are changed other than to increment the secondsElapsed var
 function startTimer() {
-  setTime();
+  setTime(60000);
 
   // We only want to start the timer if totalSeconds is > 0
   if (totalSeconds > 0) {
@@ -101,62 +100,60 @@ function startTimer() {
 
       // So renderTime() is called here once every second.
       renderTime();
-    }, 1000);
+    }, 10000);
   } else {
-    alert("Minutes of work/rest must be greater than 0.")
+    alert("Seconds left must be greater than 0.")
   }
-  var BeginEl = document.querySelector("#Begin");
-  var scoreEl = document.querySelector("#score");
-  BeginEl.addEventListener('click', startTimer);
+ 
 
   // The array of questions for our quiz game.
   var questions = [
-    {
-      prompt: "Mustang Shelby debuted in what Year?",
-      "a": "1968"
-     "b": "1998"
-     "c": "1940"
-     "d": "1965"
-     correct: "d"
+  //   {
+  //     prompt: "Mustang Shelby debuted in what Year?",
+  //     "a": "1968"
+  //    "b": "1998"
+  //    "c": "1940"
+  //    "d": "1965"
+  //    correct: "d"
 
-     prompt: "Winston Churchill was a leader in what country ?",
-      "a": "Germany"
-     "b": "Britian"
-     "c": "Russia"
-     "d": "UAE"
-     correct: "b",
+  //    prompt: "Winston Churchill was a leader in what country ?",
+  //     "a": "Germany"
+  //    "b": "Britain"
+  //    "c": "Russia"
+  //    "d": "UAE"
+  //    correct: "b",
 
-      prompt: "Denmark Vesey was a Slave in what state ?",
-      "a": "South Carolina"
-     "b": "Texas"
-     "c": "Tennesee"
-     "d": "Bermuda"
-    correct: "a"
+  //     prompt: "Denmark Vesey was a Slave in what state ?",
+  //     "a": "South Carolina"
+  //    "b": "Texas"
+  //    "c": "Tennesee"
+  //    "d": "Bermuda"
+  //   correct: "a"
 
-    prompt: "Rev. Joseph Simmons is part on what legendary musical group?",
-      `       `"a": "Milli Vanilli"
-               "b": "The Wu-tang"
-               "c": "RUN DMC"
-               "d": "Crush Groove"
-       correct: "c"
+  //   prompt: "Rev. Joseph Simmons is part on what legendary musical group?",
+  //     `       `"a": "Milli Vanilli"
+  //              "b": "The Wu-tang"
+  //              "c": "RUN DMC"
+  //              "d": "Crush Groove"
+  //      correct: "c"
 
-  prompt: "Andre Benjamin is part on what legendary musical group?",
-               "a": "OutKast"
-               "b": "The Backstreet boys"
-               "c": "Hootie and the Blowfish"
-               "d": "Nickelback"
-       correct: "a"
+  // prompt: "Andre Benjamin is part on what legendary musical group?",
+  //              "a": "OutKast"
+  //              "b": "The Backstreet boys"
+  //              "c": "Hootie and the Blowfish"
+  //              "d": "Nickelback"
+  //      correct: "a"
 
-  prompt: "This 80's show featured a talking car?",
-    "a": "Walker Texas Ranger"
-               "b": "The Renegade"
-               "c": "C.H.I.P.S."
-               "d": "Night Rider"
-       "correct": "c"
+  // prompt: "This 80's show featured a talking car?",
+  //   "a": "Walker Texas Ranger"
+  //              "b": "The Renegade"
+  //              "c": "C.H.I.P.S."
+  //              "d": "Night Rider"
+  //      "correct": "c"
 
   ]
 }
-WHEN I click the start button
+// WHEN I click the start button
 BeginEl.addEventListener("click", function (event) {
   event.preventDefault();
 
@@ -183,4 +180,4 @@ BeginEl.addEventListener("click", function (event) {
   }
 
   // Show total at end
-  alert("You got " + score + "/" + questions.length);
+  alert("You got " + score + "/" + questions.length)})}
